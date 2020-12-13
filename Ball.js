7167df139b89276b1cc50bb41bb0ac3c85258137
Nayan11111 <72578977+Nayan11111@users.noreply.github.com>
2020-12-13 14:07:10 -0500

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,20 @@
+class Ball{
+    constructor (x,y,w,h){
+        var options = {
+            density : 1,
+            frictionAir : 0.005
+        }
+        this.body = Bodies.rectangle(x,y,w,h, options);
+        this.width = w;
+        this.height = h;
+        World.add(world, this.body);
+    }
+    display(){
+        var angle = this.body.angle;
+        push ();
+        translate (this.body.position.x, this.body.position.y );
+        rotate (angle);
+        ellipse (0,0,this.width, this.height);
+        pop ();
+    }
+}
